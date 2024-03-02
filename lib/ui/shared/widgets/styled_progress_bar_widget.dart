d96d2eebe84bef4a40ff7c1c11db7af9144d388a
Eeshe <wmanuel.mc@gmail.com>
2024-03-01 23:44:06 -0400

    feat: Added border to styled progress bars

--- lib/ui/shared/widgets/styled_progress_bar_widget.dart
+++ lib/ui/shared/widgets/styled_progress_bar_widget.dart
@@ -14,14 +14,23 @@ class StyledProgressBarWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return LinearPercentIndicator(
-      animation: true,
-      animateFromLastPercent: true,
-      barRadius: const Radius.circular(2),
-      lineHeight: lineHeight,
-      progressColor: Theme.of(context).colorScheme.secondary,
-      percent: filledPercentage,
-      center: center,
+    return Container(
+      decoration: BoxDecoration(
+        border:
+            Border.all(color: Theme.of(context).colorScheme.primary, width: 1),
+        borderRadius: BorderRadius.circular(2),
+      ),
+      child: LinearPercentIndicator(
+        padding: EdgeInsets.zero,
+        animation: true,
+        animateFromLastPercent: true,
+        barRadius: const Radius.circular(2),
+        lineHeight: lineHeight,
+        progressColor: Theme.of(context).colorScheme.primary,
+        backgroundColor: Theme.of(context).colorScheme.background,
+        percent: filledPercentage,
+        center: center,
+      ),
     );
   }
 }
